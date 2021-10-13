d1f5e800365bca6fd3660bee11a35ca5a2e7a818
Gregory Brutsche <gregory@mail.brutsches.local>
2021-10-13 15:46:12 -0400

    Final touches for the workshop update.

--- Plugin.cs
+++ Plugin.cs
@@ -1,13 +1,12 @@
 ﻿using Base_Mod;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Passive_HP_Recovery {
     [UsedImplicitly]
     public class Plugin : BaseGameMod {
-        protected override string ModName => "Passive-HP-Recovery";
-
-        public override void OnGameLoaded() {
+        public override void OnGameLoaded(Scene scene) {
             foreach (var player in Resources.FindObjectsOfTypeAll<Player>()) {
                 //if (player.TryGetComponentSafe(out Health health)) {
                 //    Debug.Log($"Player health: {health.MaxHP}");
@@ -17,7 +16,7 @@ namespace Passive_HP_Recovery {
                 }
             }
 
-            base.OnGameLoaded();
+            base.OnGameLoaded(scene);
         }
     }
 }
