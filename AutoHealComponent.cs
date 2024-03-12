4995a696273782629d457f43c45fc5c0048a4aeb
Gregory Brutsche <gregory@mail.brutsches.local>
2024-03-12 17:36:06 -0500

    Update for Volc 1.31.

--- AutoHealComponent.cs
+++ AutoHealComponent.cs
@@ -1,15 +1,15 @@
 ﻿using JetBrains.Annotations;
 using UnityEngine;
 
-namespace Passive_HP_Recovery {
-    public class AutoHealComponent : MonoBehaviour {
-        [UsedImplicitly]
-        public void Update() {
-            if (!gameObject.TryGetComponentSafe(out Health healthComponent)) return;
+namespace Passive_HP_Recovery;
 
-            if (healthComponent.CurrentHP < healthComponent.MaxHP && !healthComponent.IsDead) {
-                healthComponent.CurrentHP += 8 * Time.deltaTime;
-            }
+public class AutoHealComponent : MonoBehaviour {
+    [UsedImplicitly]
+    public void Update() {
+        if (!gameObject.TryGetComponentSafe(out Health healthComponent)) return;
+
+        if (healthComponent.CurrentHP < healthComponent.MaxHP && !healthComponent.IsDead) {
+            healthComponent.CurrentHP += 8 * Time.deltaTime;
         }
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -3,20 +3,20 @@ using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace Passive_HP_Recovery {
-    [UsedImplicitly]
-    public class Plugin : BaseGameMod {
-        public override void OnGameLoaded(Scene scene) {
-            foreach (var player in Resources.FindObjectsOfTypeAll<Player>()) {
-                //if (player.TryGetComponentSafe(out Health health)) {
-                //    Debug.Log($"Player health: {health.MaxHP}");
-                //}
-                if (!player.gameObject.HasComponent<AutoHealComponent>()) {
-                    player.gameObject.AddComponent<AutoHealComponent>();
-                }
-            }
+namespace Passive_HP_Recovery;
 
-            base.OnGameLoaded(scene);
+[UsedImplicitly]
+public class Plugin : BaseGameMod {
+    public override void OnGameLoaded(Scene scene) {
+        foreach (var player in Resources.FindObjectsOfTypeAll<Player>()) {
+            //if (player.TryGetComponentSafe(out Health health)) {
+            //    Debug.Log($"Player health: {health.MaxHP}");
+            //}
+            if (!player.gameObject.HasComponent<AutoHealComponent>()) {
+                player.gameObject.AddComponent<AutoHealComponent>();
+            }
         }
+
+        base.OnGameLoaded(scene);
     }
 }
